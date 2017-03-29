9159892a5beda55b62388cfb787d96f7cd7b9b3d
turtle89431 <walterwinser@walters-MacBook.local>
2017-03-28 21:44:20 -0700

    testHomeNetwork

---
+++ lights.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walterwinser
+ * Date: 3/28/17
+ * Time: 9:22 PM
+ */
+$lights = [16,5,4,0,2,14,12,13,15,3,1];
+$useron=($_GET['on'])?$_GET['on']:null;
+$pins;
+if($useron){
+    $pins=explode(",",$useron);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="get">
+    <div>0<input type="checkbox" name="on[]" id="">1<input type="checkbox" name="on[]" id="">2<input type="checkbox" name="on[]"
+                                                                                              id=""></div>
+    <div>3<input type="checkbox" name="on[]" id="">4<input type="checkbox" name="on[]" id="">5<input type="checkbox" name=""
+                                                                                              id=""></div>
+    <div>6<input type="checkbox" name="" id="">7<input type="checkbox" name="" id="">8<input type="checkbox" name=""
+                                                                                              id=""></div>
+</form>
+</body>
+</html>
